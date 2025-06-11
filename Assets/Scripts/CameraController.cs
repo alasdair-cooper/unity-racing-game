bea2a26f12d561343f93de524f2cc89b2a6a516f
Alasdair Cooper <alasdair.code@hotmail.com>
2025-06-11 21:39:24 +0100

    Move camera back to offset after a few seconds without inputs

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -3,61 +3,59 @@ using UnityEngine.InputSystem;
 
 public class CameraController : MonoBehaviour
 {
-    public int Offset = 5;
-    public Vector2 Sensitivity = Vector2.one;
-    public float ReOrientateSpeed = 0.5f;
-    public float SecondsUntilReOrientation = 2;
+    public Vector3 Offset;
+    public Vector2 Sensitivity;
+    public float ReOrientateSpeed;
+    public float SecondsUntilReOrientation;
 
     private InputAction lookAction;
-    private InputAction jumpAction;
     private Camera Camera;
     public Component Player;
 
     private float timeWithoutInput;
 
+    private Vector3 InitialOffset() => Offset;
+
     private void Start()
     {
         lookAction = InputSystem.actions.FindAction("look");
-        jumpAction = InputSystem.actions.FindAction("jump");
         Camera = GetComponent<Camera>();
-        Camera.transform.position = Vector3.back * Offset;
+        Camera.transform.position = InitialOffset();
     }
 
     private void Update()
     {
         var delta = lookAction.ReadValue<Vector2>();
 
-
-
-        // Translate to origin
-        // Rotate 
-        // Calculate new position based on forward + follow distance
-
         if (Mathf.Approximately(delta.magnitude, 0))
         {
-            // if (timeWithoutInput > SecondsUntilReOrientation && !Mathf.Approximately(Vector3.Dot(Player.transform.up, horizontalAxis), 0))
-            // {
-            //     var rot = Quaternion.RotateTowards(Camera.transform.rotation, Quaternion.FromToRotation(Player.transform.position, Camera.transform.position), ReOrientateSpeed);
+            timeWithoutInput += Time.deltaTime;
 
-            //     Camera.transform.rotation = rot;
-            // }
+            var offset = Camera.transform.position - Player.transform.position;
 
-            // timeWithoutInput += Time.deltaTime;
+            if (offset != InitialOffset() && timeWithoutInput > SecondsUntilReOrientation)
+            {
+                var rotation = Quaternion.RotateTowards(Quaternion.identity, Quaternion.FromToRotation(offset, InitialOffset()), ReOrientateSpeed * Time.deltaTime);
+
+                Camera.transform.position = Player.transform.position + rotation * offset;
+                Camera.transform.LookAt(Player.transform.position);
+
+                timeWithoutInput += Time.deltaTime;
+            }
+
+            return;
         }
-        else
+
         {
-            Vector3 offset = Camera.transform.position - Player.transform.position;
+            var offset = Camera.transform.position - Player.transform.position;
+            var horizontalAxis = Vector3.Cross(Camera.transform.up, offset);
+            var rotation = Quaternion.AngleAxis(delta.x * Sensitivity.x, Player.transform.up) * Quaternion.AngleAxis(delta.y * Sensitivity.y, horizontalAxis);
 
-            offset = Quaternion.AngleAxis(delta.x * Sensitivity.x, Player.transform.up) * offset;
-
-            var horizontalAxis = Vector3.Cross(Player.transform.up, offset);
-
-            offset = Quaternion.AngleAxis(delta.y * Sensitivity.y, horizontalAxis) * offset;
-
-            Camera.transform.position = Player.transform.position + offset;
+            Camera.transform.position = Player.transform.position + rotation * offset;
             Camera.transform.LookAt(Player.transform.position);
 
             timeWithoutInput = 0;
         }
+
     }
 }
