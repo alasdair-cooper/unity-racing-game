06d83b778faccc3dd45484a3757bbe882b977e18
Alasdair Cooper <alasdair.code@hotmail.com>
2025-06-23 23:07:29 +0100

    Rotate wheels to match steering lock

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,26 +10,24 @@ public class PlayerController : MonoBehaviour
     public float AirResistanceMultiplier;
 
     public float TurningSpeed;
+    public float FullLockAngle;
 
     private InputAction accelerateAction;
     private InputAction brakeAction;
     private InputAction moveAction;
 
-    private Transform playerTransform;
-
     private float currentSpeed = 0;
+    private float currentLockAngle = 0;
 
     private float AirResistanceDeceleration => 1 / AirResistanceMultiplier * currentSpeed / EngineAcceleration;
 
-    private float SteeringAngleDegrees => moveAction.ReadValue<Vector2>().x * 45;
+    public float SteeringAngle => currentLockAngle;
 
     void Start()
     {
         accelerateAction = InputSystem.actions.FindAction("accelerate");
         brakeAction = InputSystem.actions.FindAction("brake");
         moveAction = InputSystem.actions.FindAction("move");
-
-        playerTransform = GetComponent<Transform>();
     }
 
     void Update()
@@ -38,8 +36,23 @@ public class PlayerController : MonoBehaviour
 
         currentSpeed = Mathf.Max(0, currentSpeed + currentAcceleration);
 
-        playerTransform.Rotate(0, SteeringAngleDegrees * Time.deltaTime * TurningSpeed, 0);
+        var moveInputValue = moveAction.ReadValue<Vector2>().x;
 
-        playerTransform.position += currentSpeed * Time.deltaTime * playerTransform.forward;
+        if (moveInputValue != 0)
+        {
+            currentLockAngle = Mathf.Clamp(SteeringAngle + moveAction.ReadValue<Vector2>().x * Time.deltaTime * TurningSpeed, -FullLockAngle, FullLockAngle);
+        }
+        else if (SteeringAngle > 0)
+        {
+            currentLockAngle = Mathf.Clamp(SteeringAngle - Time.deltaTime * TurningSpeed, 0, FullLockAngle);
+        }
+        else if (SteeringAngle < 0)
+        {
+            currentLockAngle = Mathf.Clamp(SteeringAngle + Time.deltaTime * TurningSpeed, -FullLockAngle, 0);
+        }
+
+        // playerTransform.Rotate(0, SteeringAngleDegrees * Time.deltaTime * TurningSpeed, 0);
+
+        transform.position += currentSpeed * Time.deltaTime * transform.forward;
     }
 }
